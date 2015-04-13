ce66e3847c37d10ac5015d0288341e6a68ee881f
rgkavodkar <rg.kavodkar@gmail.com>
2015-04-13 05:29:17 -0400

    Updated client and server code

--- client/client.py
+++ client/client.py
@@ -1,13 +1,39 @@
 __author__ = 'rg.kavodkar'
 import socket
 import util.constants as constants
+import logging
+from client import client_logger_init
 
+# TODO: Put the whole functionality into a function
+
+# Initialize the client logger
+client_logger_init.init()
+
+# Get the logger object
+logger = logging.getLogger("client_log")
+
+# Get the client hostname
 host = socket.gethostname()
 
-
+# Create a client TCP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# TODO: Assuming that the server is running on the same machine, needs to be changed
+# Connect to the server at the given address
 client_socket.connect((host, constants.SERVER_PORT))
-client_socket.send("Hello World!".encode(constants.ENCODING))
-message = client_socket.recv(constants.MAX_BUFFER_SIZE)
-print("Server message:", str(message, constants.ENCODING))
-client_socket.close()
+
+# Keep alive connection
+while True:
+    client_request = input("Enter Command: ")
+    if client_request == constants.CMD_EXIT:
+        # Received command for exit, closing the socket
+        logger.info("Closing the client socket and the connection")
+        client_socket.close()
+        break
+
+    # Send the server the command
+    client_socket.send(bytes(client_request, constants.ENCODING))
+    logger.info("Request to server: " + client_request)
+    # Receive the message from server
+    message = client_socket.recv(constants.MAX_BUFFER_SIZE)
+    logger.info("Message from server: " + str(message, constants.ENCODING))
--- server/server.py
+++ server/server.py
@@ -1,22 +1,58 @@
 __author__ = 'rg.kavodkar'
 
 import socket
+import socketserver
 import util.constants as constants
+import logging
+from server import server_logger_init
 
-# Assuming the server is running on the same machine
-# Needs to be changed when deploying on a different server
+# TODO: Make the whole thing like a function
+
+# Initialize the server logger
+server_logger_init.init()
+
+# Get the server logger object created in the logger init
+logger = logging.getLogger("server_log")
+
+# Get the hostname of this machine
 host = socket.gethostname()
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((host, constants.SERVER_PORT))
 
-server_socket.listen(5)
+# A handler class whose instance is created for each client that connects
+class RequestHandler(socketserver.BaseRequestHandler):
+    # Overriding the method
+    def handle(self):
+        client_host = self.client_address[0]
+        client_port = self.client_address[1]
+        logger.info("Client Connected: " + client_host)
 
-while 1:
-    client_socket, address = server_socket.accept()
-    client_data = str(client_socket.recv(constants.MAX_BUFFER_SIZE), constants.ENCODING)
-    print("Client Message:", client_data)
-    client_socket.send("Hello World!".encode(constants.ENCODING))
-    client_socket.close()
+        while 1:
+            # Receive the client request
+            client_request = str(self.request.recv(constants.MAX_BUFFER_SIZE), constants.ENCODING)
+
+            # If received null, break and close the socket
+            if not client_request:
+                logger.warn("Closing the connection with " + client_host)
+                break
+
+            # Performing strip after above check to avoid "" as a potential socket terminator
+            client_request = client_request.strip()
+            logger.info("Request from client: " + client_request)
+
+            # Response for client
+            response = client_request.upper()
+            logger.info("Response to client: " + response)
+            self.request.send(bytes(response, "utf-8"))
+
+        logger.info("Client Disconnected: " + client_host)
 
 
+# An instance of the ThreadedTCPServer class that handles the threading for each client
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+# Create an instance of ThreadedTCPServer
+server = ThreadedTCPServer((host, constants.SERVER_PORT), RequestHandler)
+logger.info("Starting main server")
+server.serve_forever()
